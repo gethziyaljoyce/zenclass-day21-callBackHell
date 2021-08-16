16d8c4667d50b30a799578094d1c7ec2b52e4782
gethziyaljoyce <87714086+gethziyaljoyce@users.noreply.github.com>
2021-08-16 20:29:27 +0530

    Add files via upload

---
+++ callBackHell.js
@@ -0,0 +1,36 @@
+setTimeout(()=>{
+   $('#one').append("10");
+    setTimeout(()=>{
+      $("#two").append("9");
+        setTimeout(()=>{
+         $("#three").append("8");
+            setTimeout(()=>{
+               $("#four").append("7");
+                setTimeout(()=>{
+                  $("#five").append("6");
+                    setTimeout(()=>{
+                     $("#six").append("5");
+                        setTimeout(()=>{
+                           $("#seven").append("4");
+                            setTimeout(()=>{
+                              $("#eight").append("3");
+                                setTimeout(()=>{
+                                 $("#nine").append("2");
+                                    setTimeout(()=>{
+                                       $("#ten").append("1");
+                                        setTimeout(()=>{
+                                          $("#final").append("Happay New Year !!!");
+                                        },3000)
+                                    },2000)
+                                },2000)
+                            },2000)
+                        },2000)
+                    },2000)
+                },2000)
+            },2000)
+        },2000)
+    },2000)
+},2000)
+
+      
+   
