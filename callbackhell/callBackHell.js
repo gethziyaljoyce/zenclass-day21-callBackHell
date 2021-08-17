dfb809904ad45614d51d2d7d6494deb9889648e3
gethziyaljoyce <87714086+gethziyaljoyce@users.noreply.github.com>
2021-08-17 09:53:43 +0530

    Add files via upload

---
+++ callbackhell/callBackHell.js
@@ -0,0 +1,36 @@
+
+setTimeout(() => {
+  document.getElementById("one").innerHTML = "10";
+  setTimeout(() => {
+    document.getElementById("one").innerHTML = "9";
+    setTimeout(() => {
+      document.getElementById("one").innerHTML = "8";
+      setTimeout(() => {
+        document.getElementById("one").innerHTML = "7";
+        setTimeout(() => {
+          document.getElementById("one").innerHTML = "6";
+          setTimeout(() => {
+            document.getElementById("one").innerHTML = "5";
+            setTimeout(() => {
+              document.getElementById("one").innerHTML = "4";
+              setTimeout(() => {
+                document.getElementById("one").innerHTML = "3";
+                setTimeout(() => {
+                  document.getElementById("one").innerHTML = "2";
+                  setTimeout(() => {
+                    document.getElementById("one").innerHTML = "1";
+                    setTimeout(() => {
+                      document.getElementById("one").innerHTML = "Happy New Year!!!!";
+                    }, 2000);
+                  }, 2000);
+                }, 2000);
+              }, 2000);
+            }, 2000);
+          }, 2000);
+        }, 2000);
+      }, 2000);
+    }, 2000);
+  }, 2000);
+}, 2000);
+
+
